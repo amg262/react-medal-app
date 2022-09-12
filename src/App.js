2b63a6a8a2f58c6a82b3ba0d2dd9af82a1f613da
Andrew <andrewmgunn26@gmail.com>
2022-09-12 11:20:31 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Country from './components/Country';
+import './App.css';
+
+class App extends Component {
+  state = {
+    countries: [
+      { id: 1, name: 'United States', gold: 2 },
+      { id: 2, name: 'China', gold: 3 },
+      { id: 3, name: 'Germany', gold: 0 },
+    ]
+  }
+  render() { 
+    return (
+      <React.Fragment>
+        { this.state.countries.map(country => 
+          <Country key={ country.id } country={ country } />
+        )}
+      </React.Fragment>
+    );
+  }
+}
+ 
+export default App;
